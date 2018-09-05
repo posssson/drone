1967b2e24bc56482124289844ad5d8c02a8b34c6
posssson <40440685+posssson@users.noreply.github.com>
2018-09-05 19:43:59 +0200

    Modification pour améliorer la lecture du code (servos)

--- src/Servos/Servos.cpp
+++ src/Servos/Servos.cpp
@@ -18,7 +18,7 @@ double time_in_mill = 0, temps_proc = 0;
 
 struct timeval  tv;
 
-
+	// Fonctions de communication ROS
 	boost::function<void(const drone::Capteurs_msg::ConstPtr& msg)> temp = boost::bind(recuperation_capteur, _1, &recu_init, attitude);
 	ros::Subscriber sub = n.subscribe("capteurs", 1, temp);
 
@@ -31,7 +31,10 @@ struct timeval  tv;
 	drone::Erreur_angle_msg msg_erreur_angle;
 	ros::Publisher _pub_msg_erreur_angle = n.advertise<drone::Erreur_angle_msg>("erreur_angle", 1);
 
+	// Ouverture d'un fichier pour stocker les données
 	ofstream fichier("/home/pi/drone_ws/src/drone/src/donnees.txt", ios::out | ios::trunc); 
+	
+	// Initialisation des gpio
 	if (gpioInitialise() < 0)
 	{
 		printf("BUG\n");
@@ -49,25 +52,16 @@ struct timeval  tv;
 	gpioSetAlertFunc(26, recuperation_cmd_roulis);
 	gpioSetAlertFunc(12, recuperation_cmd_arret);
 
-
-	// initialisation moteur
-	int moteur_devant_gauche = 27;
-	int moteur_deriere_gauche = 17;
-	int moteur_deriere_droit = 5;
-	int moteur_devant_droit = 6;
 	gpioSetMode(moteur_devant_gauche, PI_OUTPUT);
 	gpioSetMode(moteur_deriere_gauche, PI_OUTPUT);
 	gpioSetMode(moteur_deriere_droit, PI_OUTPUT);
 	gpioSetMode(moteur_devant_droit, PI_OUTPUT);
-
-
 
 	gpioSetPWMrange(moteur_devant_droit,2000);
 	gpioSetPWMrange(moteur_devant_gauche, 2000);
 	gpioSetPWMrange(moteur_deriere_gauche, 2000);
 	gpioSetPWMrange(moteur_deriere_droit, 2000);
 	
-
 	gpioSetPWMfrequency(moteur_devant_droit, 500);
 	gpioSetPWMfrequency(moteur_devant_gauche, 500);
 	gpioSetPWMfrequency(moteur_deriere_gauche, 500);
@@ -80,18 +74,6 @@ struct timeval  tv;
 	gpioPWM(moteur_devant_droit, 1000);
 	// fin initialisation moteur
 
-	// initialisation variable PID
-	int roulis = 1, tangage =0 ,lacet = 2;
-
-	float gaz = 1100;
-	float commande_devant_droit = 0, commande_devant_gauche = 0, commande_derierre_gauche = 0, commande_derierre_droit = 0;
-	float arret = 0;
-	int raz = 0;
-	float init_lacet = 0;
-	int erreur_max = 1;
- struct timespec time_actuel,ancien_temps,temps_attente,temps_attente_nanosleep;
-	int initialisation_lacet = 0;
-
 	nanosleep(&temps_attente,&temps_attente_nanosleep);
 	
 	temps_attente.tv_sec = 0;
@@ -101,34 +83,7 @@ struct timeval  tv;
 	while (ros::ok())
 	{
 		
-		clock_gettime( CLOCK_REALTIME, &time_actuel);
-		temps_proc =(time_actuel.tv_sec - ancien_temps.tv_sec) + (time_actuel.tv_nsec - ancien_temps.tv_nsec)/1000000000.0;
-		
-		while (temps_proc < 0.002)
-		{
-			clock_gettime( CLOCK_REALTIME, &time_actuel);
-			temps_proc =(time_actuel.tv_sec - ancien_temps.tv_sec) + (time_actuel.tv_nsec - ancien_temps.tv_nsec)/1000000000.0;
-			nanosleep(&temps_attente,&temps_attente_nanosleep);
-		}
-		ancien_temps = time_actuel;
-		
-		//ROS_INFO("temps vrai = %lf ", temps_proc);
-	
-
-		// ajustement PID en fonction du temps
-		for (int i=0;i<3;i++) {
-			
-			Ki[i] = Ki_default[i]*(float)temps_proc;
-			Ki_vit[i] = Ki_vit_default[i]*(float)temps_proc;
-
-			
-			Kd[i] = Kd_default[i]/(float)temps_proc;
-			Kd_vit[i] = Kd_vit_default[i]/(float)temps_proc;
-
-		}
-
-
-		// calcul PID
+		// Initialisation vrai quand tout est initialisé
 		if (recu_init == 1)
 		{
 			if (initialisation_lacet == 0 && abs(attitude[tangage])<5 && abs(attitude[roulis])<5)
@@ -138,22 +93,35 @@ struct timeval  tv;
 				init_lacet = attitude[lacet];
 				initialisation_lacet = 1;
 			}
+		// Boucle permettant d'avoir un temps de cycle de 0.002 (500 Hz)
+		clock_gettime( CLOCK_REALTIME, &time_actuel);
+		temps_proc =(time_actuel.tv_sec - ancien_temps.tv_sec) + (time_actuel.tv_nsec - ancien_temps.tv_nsec)/1000000000.0;
+		while (temps_proc < 0.002)
+		{
+			clock_gettime( CLOCK_REALTIME, &time_actuel);
+			temps_proc =(time_actuel.tv_sec - ancien_temps.tv_sec) + (time_actuel.tv_nsec - ancien_temps.tv_nsec)/1000000000.0;
+			nanosleep(&temps_attente,&temps_attente_nanosleep);
+		}
+		ancien_temps = time_actuel;
+		
+		// ajustement PID en fonction du temps
+		for (int i=0;i<3;i++) {		
+			Ki[i] = Ki_default[i]*(float)temps_proc;
+			Ki_vit[i] = Ki_vit_default[i]*(float)temps_proc;
+			Kd[i] = Kd_default[i]/(float)temps_proc;
+			Kd_vit[i] = Kd_vit_default[i]/(float)temps_proc;
+		}
 
-
-
-
-// Calcul PID GAZ
+			// Calcul PID Commande GAZ //TODO
 			gaz += (-gaz_actuel + temps_haut_gaz)*0.0015;
 			//ROS_INFO("GAZ = %f",gaz);
 			gaz_actuel = gaz;
 
-			// PID gaz
-
-
-			// 
+			// Calcul PID Commande roulis/tangage
 			consigne[roulis] =((float)temps_haut_roulis- 1500.0)/10;
 			consigne[tangage] =-((float)temps_haut_tangage - 1500.0)/10;
 			
+			// Mise à zéro si test sans manette
 			//consigne[tangage] = 0;
 			//consigne[roulis] = 0;
 
@@ -167,36 +135,12 @@ struct timeval  tv;
 				raz = 1;
 			}
 
+			// Limite de gaz
 			if (gaz > 1900)
 				gaz = 1900;
 
-		
-			// Calcul PID angle
-		
-			erreur[tangage] = consigne[tangage] - attitude[tangage];
-			erreur[roulis] = consigne[roulis] - attitude[roulis];
-			erreur[lacet] = consigne[lacet] - (attitude[lacet] - init_lacet);
-
-			
-			somme_erreurs[tangage] +=  erreur[tangage];
-			somme_erreurs[tangage] *= raz;
-			somme_erreurs[roulis] += erreur[roulis];
-			somme_erreurs[roulis] *= raz;
-			somme_erreurs[lacet] += erreur[lacet];
-			somme_erreurs[lacet] *= raz;
-
-			variation_erreur[tangage] = erreur[tangage] - erreur_precedente[tangage];
-			variation_erreur[roulis] = erreur[roulis] - erreur_precedente[roulis];
-			variation_erreur[lacet] = erreur[lacet] - erreur_precedente[lacet];
-
-
-			commande[tangage] =Kp[tangage] * erreur[tangage] + Ki[tangage] * somme_erreurs[tangage] + Kd[tangage] * variation_erreur[tangage];
-			commande[roulis] = Kp[roulis] * erreur[roulis] + Ki[roulis] * somme_erreurs[roulis] + Kd[roulis] * variation_erreur[roulis];
-			commande[lacet] = Kp[lacet] * erreur[lacet] + Ki[lacet] * somme_erreurs[lacet] + Kd[lacet] * variation_erreur[lacet];
-
-			erreur_precedente[tangage] = erreur[tangage];
-			erreur_precedente[roulis] = erreur[roulis];
-			erreur_precedente[lacet] = erreur[lacet];
+			// Calcul PID Angles
+			calcul_pid_angle();
 			
 			// Saturation de la commande en angle.
 			commande[tangage] = saturation(commande[tangage],-35,35);
@@ -208,48 +152,24 @@ struct timeval  tv;
 			//commande[roulis] = 0;
 
 			// Calcul PID vitesse
-			// TODO ne pas laisser 3 et 2
-			erreur_vit[tangage] = commande[tangage] - attitude[3];
-			erreur_vit[roulis] = commande[roulis] - attitude[4];
-			erreur_vit[lacet] = commande[lacet] - 0;
-
+			calcul_pid_vitesse();
 			
-
-			somme_erreurs_vit[tangage] +=  erreur_vit[tangage];
-			somme_erreurs_vit[tangage] *= raz;
-			somme_erreurs_vit[roulis] += erreur_vit[roulis];
-			somme_erreurs_vit[roulis] *= raz;
-			somme_erreurs_vit[lacet] += erreur_vit[lacet];
-			somme_erreurs_vit[lacet] *= raz;
-
-			variation_erreur_vit[tangage] = erreur_vit[tangage] - erreur_precedente_vit[tangage];
-			variation_erreur_vit[roulis] = erreur_vit[roulis] - erreur_precedente_vit[roulis];
-			variation_erreur_vit[lacet] = erreur_vit[lacet] - erreur_precedente_vit[lacet];
-
-
-			commande_vit[tangage] = Kp_vit[tangage] * erreur_vit[tangage] + Ki_vit[tangage] * somme_erreurs_vit[tangage] + Kd_vit[tangage] * variation_erreur_vit[tangage];
-			commande_vit[roulis] = Kp_vit[roulis] * erreur_vit[roulis] + Ki_vit[roulis] * somme_erreurs_vit[roulis] + Kd_vit[roulis] * variation_erreur_vit[roulis];
-			commande_vit[lacet] = Kp_vit[lacet] * erreur_vit[lacet] + Ki_vit[lacet] * somme_erreurs_vit[lacet] + Kd_vit[lacet] * variation_erreur_vit[lacet];
-
-			erreur_precedente_vit[tangage] = erreur_vit[tangage];
-			erreur_precedente_vit[roulis] = erreur_vit[roulis];
-			erreur_precedente_vit[lacet] = erreur_vit[lacet];
-			
-
+			// TODO à enlever
 			//commande_vit[tangage] = 0;
 			//commande_vit[roulis] = 0;
 			//commande_vit[lacet] = 0;
 			commande[lacet] = 0;
 
+			// Saturation commadne tangage et roulis
 			commande_vit[tangage] = saturation(commande_vit[tangage],-250,250);
 			commande_vit[roulis] = saturation(commande_vit[roulis],-250,250);
 			
+			// Boucle pour afficher des valeurs
 			it++;
 			if (it ==200) {			
 				it = 0;
 				
 			}
-
 			if (it == 0)
 			{
 			//ROS_INFO("erreur[roulis] =  %f : erreur_vit[roulis] = %f  commande_vit[roulis] = %f  ",erreur[roulis], erreur_vit[roulis],commande_vit[roulis]);
@@ -272,6 +192,7 @@ struct timeval  tv;
 			//ROS_INFO("gaz = %f",gaz);
 			}
 			
+			
 			// security
 			if (arret == 0 && gaz <1100)
 			{
@@ -279,49 +200,10 @@ struct timeval  tv;
 				ROS_INFO("ENLEVER SECURITE");
 			}
 
-			// Ajout de la commande de gaz
-			commande_derierre_droit = gaz;
-			commande_derierre_gauche = gaz;
-			commande_devant_droit = gaz;
-			commande_devant_gauche = gaz;
+			calcul_valeur_commande_moteur();
 
-			// Mise en place de la commadne en fonction positif négatif
-			if (commande_vit[tangage] < 0)
-			{
-				commande_devant_droit -= commande_vit[tangage];
-				commande_devant_gauche -= commande_vit[tangage];
-			}
-			if (commande_vit[roulis] > 0)
-			{
-
-				commande_derierre_droit += commande_vit[roulis];
-				commande_devant_droit += commande_vit[roulis];
-			}
-			if (commande_vit[tangage] > 0)
-			{
-				commande_derierre_droit += commande_vit[tangage];
-				commande_derierre_gauche += commande_vit[tangage];
-			}
-			if (commande_vit[roulis] < 0)
-			{
-				commande_derierre_gauche -= commande_vit[roulis];
-				commande_devant_gauche -= commande_vit[roulis];
-			}
-
-			commande_devant_gauche -= commande[lacet];
-			commande_derierre_droit -= commande[lacet];
-
-			commande_devant_droit += commande[lacet];
-			commande_derierre_gauche += commande[lacet];
-
-			// Saturation de la commande
-			commande_devant_droit = saturation(commande_devant_droit ,1150,1900);
-			commande_devant_gauche = saturation(commande_devant_gauche,1150,1900);
-			commande_derierre_droit = saturation(commande_derierre_droit,1150,1900);
-			commande_derierre_gauche = saturation(commande_derierre_gauche,1150,1900); 
-
-			
-			if ((abs( attitude[tangage]) > 60 || abs( attitude[roulis])>60) && arret == 1)
+			// Sécurité sur les angles pour les tests 
+			if ((abs( attitude[tangage]) > 15 || abs( attitude[roulis])>15) && arret == 1)
 				{
 					// SECURITE DE TEST
 					/*ROS_INFO("MISE EN SECURITE");
@@ -337,13 +219,11 @@ struct timeval  tv;
 				gpioPWM(moteur_devant_gauche, commande_devant_gauche);  // Moyenement OK
 				gpioPWM(moteur_deriere_droit, commande_derierre_droit);   // OK
 				gpioPWM(moteur_deriere_gauche, commande_derierre_gauche);
-				
-				
-
-			fichier << " " << attitude[tangage] << " " << attitude[roulis] << " "<<attitude[4] << " " << attitude[3]  << " " << commande[tangage]<<" " << commande[roulis] << "  " << commande_vit[tangage] << " " << commande_vit[roulis] << endl;
+				// Inscription dans un fichier les valeurs
+				fichier << " " << attitude[tangage] << " " << attitude[roulis] << " "<<attitude[4] << " " << attitude[3]  << " " << commande[tangage]<<" " << commande[roulis] << "  " << commande_vit[tangage] << " " << commande_vit[roulis] << endl;
 			}
 
-			else
+			else // On bloque les moteurs
 			{
 				optim = 1;
 				gpioPWM(moteur_devant_gauche, 1000);
