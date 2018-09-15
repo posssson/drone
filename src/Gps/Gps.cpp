021349c2dc523a89b36b50c7564ceee417acf08c
posssson <40440685+posssson@users.noreply.github.com>
2018-09-15 11:21:55 +0200

    add gps uart

--- src/Gps/Gps.cpp
+++ src/Gps/Gps.cpp
@@ -7,36 +7,57 @@
 #include <stdint.h>
 #include <string.h>
 #include "std_msgs/String.h"
-#include "serial.h"
+//#include "serial.h"
+#include <wiringSerial.h>
 using namespace std;
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
 int messagerecu =0;
+int fd,i ;
+int nbr;
+int length = 0;
+string dollar_test = "$";
+string gps_message;
+string char_gps;
 
-SimpleSerial * serial;
+//SimpleSerial * serial;
 // %Tag(CALLBACK)%
+void gps();
 void gps()
 {
-	int ser;
-	char ok;
-	string gps_message_uart = "$GPGGA,095455.40,,,,,0,00,99.99,,,,,,*6A";	
-std::string delimiter = ",";
-
-//gps_message_uart = serial->readLine();
-cout << "i heard normal " << gps_message_uart << endl;
-
-// On parse les résultats
-size_t pos = 0;
-std::string token;
-while ((pos = gps_message_uart.find(delimiter)) != std::string::npos) {
-    token = gps_message_uart.substr(0, pos);
-    std::cout << token << std::endl;
-    gps_message_uart.erase(0, pos + delimiter.length());
-}
 
 
 
+	i=0;
+	while (1) // Tant que la valeur est pas $
+	{
+		char_gps = (char)serialGetchar(fd);
+		if (char_gps.compare(dollar_test)==0)
+		{
+			char_gps.clear();
+			break;
+		}
+		gps_message += (char_gps);
+		char_gps.clear();
+	}
+
+	fflush (stdout) ;
+
+	std::string delimiter = ",";
+
+	// On parse les résultats
+	size_t pos = 0;
+	std::string token;
+	while ((pos = gps_message.find(delimiter)) != std::string::npos) {
+	    token = gps_message.substr(0, pos);
+	    std::cout << token << std::endl;
+	    gps_message.erase(0, pos + delimiter.length());
+	}
+
+
+	//cout<<gps_message<<endl;
+	gps_message.clear();
 
 }
 // %EndTag(CALLBACK)%
@@ -44,29 +65,29 @@ while ((pos = gps_message_uart.find(delimiter)) != std::string::npos) {
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "gps");
+	ros::init(argc, argv, "gps");
 
-  ros::NodeHandle n;
+	ros::NodeHandle n;
 
+	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("gps", 1);
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("gps", 1);
+	ROS_INFO("Init GPS ok");
+	//serial = new SimpleSerial("/dev/ttyAMA0", 9600, 100);
+	fd = serialOpen("/dev/ttyAMA0", 9600);
+	while (ros::ok())
+	{
 
-ROS_INFO("Init GPS ok");
-serial = new SimpleSerial("/dev/ttyAMA0", 9600, 100);
-  while (ros::ok())
-  {
-  usleep(10000);
-  //gps();
-
-
-    ros::spinOnce();
-
-  
-  }
+		sleep(0.2);
+		gps();
+		ROS_INFO("fin gps");
+		ros::spinOnce();
 
 
+	}
 
-  return 0;
+
+
+	return 0;
 }
 // %EndTag(FULLTEXT)%
 
